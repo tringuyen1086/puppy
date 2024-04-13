79cfb1f893eeec084382c150129ad34d0cd620f9
Tri Nguyen <71200950+tringuyen1086@users.noreply.github.com>
2024-04-13 00:38:53 -0700

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tringuyen1086/puppy
 
 go 1.22.1
+
+require github.com/tringuyen1086/dog v0.0.0-20240413073617-67b48d64d4bb // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import(
+	"github.com/tringuyen1086/dog"
+)
+
 func Bark() string{
 	return "Woof!"
 }
 
 func Barks() string{
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string{
+	return dog.WhenGrowUp(Bark())
+}
+
+func BigBarks() string{
+	return dog.WhenGrowUp(Barks())
 }
